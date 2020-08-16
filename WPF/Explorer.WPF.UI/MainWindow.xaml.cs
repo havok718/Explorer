48324d74a813a7f3b043ecba7024fbec56ea316f
Александр Бурлаков <havokrazor718@gmail.com>
2020-08-16 23:08:18 +0300

    Add project files.

---
+++ WPF/Explorer.WPF.UI/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Explorer.Shared.ViewModels;
+
+namespace Explorer.WPF.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+    }
+}
